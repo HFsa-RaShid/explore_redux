d8ca680a0f8cd9d5055c1c7d7603083135f773dc
Hafsa_Rashid <hafsa20cse028@gmail.com>
2025-01-02 22:20:11 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
 import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
 import { decrement, increment } from './redux/features/counter/counterSlice';
+import { RootState } from './redux/store';
 
 function App() {
  const dispatch = useDispatch();
- const {count} = useSelector((state) => state.counter);
+ const {count} = useSelector((state: RootState) => state.counter);
 
  const handleIncrement = () => {
   dispatch(increment());
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit"
 import CounterReducer from "./features/counter/counterSlice";
-const store = configureStore({
+export const store = configureStore({
     reducer: {
         counter: CounterReducer,
     },
 }
 );
 
-export default store;
+export type RootState = ReturnType<typeof store.getState>; //.getstate method use korle store ar sob state peye jabo
+
+export type AppDispatch = typeof store.dispatch; //store.dispatch ar type
