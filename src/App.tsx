2aa8e31e550ef8b72e446c7eb17a93ab2383502c
Hafsa_Rashid <hafsa20cse028@gmail.com>
2025-01-02 22:34:00 +0600

    done

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,20 @@ function App() {
   const dispatch = useAppDispatch();
   const { count } = useAppSelector((state) => state.counter);
 
-  const handleIncrement = () => {
-    dispatch(increment());
+  const handleIncrement = (amount: number) => {
+    dispatch(increment(amount));
   };
-  const handleDecrement = () => {
-    dispatch(decrement());
+  const handleDecrement = (amount: number) => {
+    dispatch(decrement(amount));
   };
 
   return (
     <div>
       <h1>Counter with Redux</h1>
-      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={() => handleIncrement(5)}>Increment by 5</button>
+      <button onClick={() => handleIncrement(1)}>Increment</button>
       <div>{count}</div>
-      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={() => handleDecrement(5)}>Decrement</button>
     </div>
   );
 }
